913945bb33dc9853cbe731784cf409d0dc413afd
Rebecca Le <543859+sevenseacat@users.noreply.github.com>
2024-07-09 20:37:18 +0800

    docs: Add protocol to config doc link to make it usable (#39)
    Having a fully-qualified URL lets you cmd+click and go there from your editor! (on macOS - its probably ctrl+click on Windows)

--- lib/igniter/project/igniter_config.ex
+++ lib/igniter/project/igniter_config.ex
@@ -46,7 +46,7 @@ defmodule Igniter.Project.IgniterConfig do
       ".igniter.exs",
       """
       # This is a configuration file for igniter.
-      # For option documentation, see hexdocs.pm/igniter/Igniter.Project.IgniterConfig.html
+      # For option documentation, see https://hexdocs.pm/igniter/Igniter.Project.IgniterConfig.html
       # To keep it up to date, use `mix igniter.setup`
 
       []
