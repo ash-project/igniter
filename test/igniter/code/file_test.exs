f356cce3a41bc1b70f05c4b336c1eb18c3e7e024
Mayel de Borniol <115318+mayel@users.noreply.github.com>
2024-08-06 02:50:18 +0100

    improvement: support for non-elixir files with create_new_file, update_file, include_existing_file, include_or_create_file, create_or_update_file (#75)

---
+++ test/igniter/code/file_test.exs
@@ -0,0 +1,42 @@
+defmodule Igniter.Code.FileTest do
+  use ExUnit.Case
+
+  test "files will be created if they do not exist" do
+    %{rewrite: rewrite} =
+      Igniter.new()
+      |> Igniter.create_new_file("lib/foo/bar/something.heex", """
+      <div>Hello</div>
+      """)
+      |> Igniter.prepare_for_write()
+
+    assert rewrite
+           |> Rewrite.source!("lib/foo/bar/something.heex")
+           |> Rewrite.Source.get(:content) == """
+           <div>Hello</div>
+           """
+  end
+
+  test "files will be read if they exist" do
+    assert %{rewrite: rewrite} =
+             Igniter.new()
+             |> Igniter.include_existing_file("README.md", required?: true)
+
+    assert rewrite
+           |> Rewrite.source!("README.md")
+           |> Rewrite.Source.get(:content) =~ "code generation"
+  end
+
+  test "can update file if it exists" do
+    assert %{rewrite: rewrite} =
+             Igniter.new()
+             |> Igniter.include_existing_file("README.md", required?: true)
+             |> Igniter.update_file("README.md", fn source ->
+               Rewrite.Source.update(source, :content, "Hello Test")
+             end)
+             |> Igniter.prepare_for_write()
+
+    assert rewrite
+           |> Rewrite.source!("README.md")
+           |> Rewrite.Source.get(:content) == "Hello Test"
+  end
+end
