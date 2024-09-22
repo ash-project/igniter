a49ebd7950f2d326e4087dc109c4a30b219af1a4
James Harton <james@harton.nz>
2024-09-22 12:38:45 +1200

    fix: Accept an empty string when prompting
    The prompt `[Yn]` would indicate that `Y` is the default action, therefore we should accept an empty string as an indicator that the user wants us to continue.

--- lib/igniter/util/io.ex
+++ lib/igniter/util/io.ex
@@ -4,7 +4,7 @@ defmodule Igniter.Util.IO do
   @doc "Prompts the user for yes or no, repeating the prompt until a satisfactory answer is given"
   def yes?(prompt) do
     case String.trim(Mix.shell().prompt(prompt <> " [Yn]")) do
-      yes when yes in ["y", "Y", "yes", "YES"] ->
+      yes when yes in ["y", "Y", "yes", "YES", ""] ->
         true
 
       no when no in ["n", "N", "no", "NO"] ->
