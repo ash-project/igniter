a867793246a5689a8d5c48b59737b2a642d90741
Zach Daniel <zach@zachdaniel.dev>
2024-10-21 13:51:57 -0400

    fix: properly detect all version migrations

--- lib/igniter/upgrades.ex
+++ lib/igniter/upgrades.ex
@@ -6,13 +6,10 @@ defmodule Igniter.Upgrades do
   @doc "Run all upgrades from `from` to `to`."
   def run(igniter, from, to, upgrade_map, opts) do
     upgrade_map
+    |> Enum.filter(fn {version, _} ->
+      Version.match?(version, "> #{from} and <= #{to}")
+    end)
     |> Enum.sort_by(&elem(&1, 0), &Version.compare/2)
-    |> Enum.drop_while(fn {version, _} ->
-      Version.compare(from, version) in [:gt, :eq]
-    end)
-    |> Enum.take_while(fn {version, _} ->
-      Version.compare(to, version) in [:lt, :eq]
-    end)
     |> Enum.flat_map(&elem(&1, 1))
     |> Enum.reduce(igniter, fn upgrade, igniter ->
       upgrade.(igniter, opts)
