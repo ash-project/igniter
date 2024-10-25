8f5f45dae73e04196838d79d2bd9f9f3fcfc9683
Leandro Pereira <leandro@leandro.io>
2024-10-25 14:46:30 -0400

    mix format

--- mix.exs
+++ mix.exs
@@ -104,7 +104,7 @@ defmodule Igniter.MixProject do
       {:mix_audit, ">= 0.0.0", only: [:dev, :test], runtime: false},
       {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
       {:benchee, "~> 1.1", only: [:dev, :test]},
-      {:doctor, "~> 0.21", only: [:dev, :test]},
+      {:doctor, "~> 0.21", only: [:dev, :test]}
     ]
   end
 
