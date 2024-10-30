53fba22fbe424962ee4c6010132a44fe39a4deb7
Dave Lucia <davelucianyc@gmail.com>
2024-10-30 14:01:19 -0400

    use rightmost

--- lib/igniter/project/application.ex
+++ lib/igniter/project/application.ex
@@ -89,7 +89,7 @@ defmodule Igniter.Project.Application do
       |> Sourceror.Zipper.zip()
 
     with {:ok, zipper} <- Igniter.Code.Function.move_to_def(zipper, :application, 0),
-         {:ok, zipper} <- Igniter.Code.Common.move_right(zipper, &Igniter.Code.List.list?/1),
+         {:ok, zipper} <- Igniter.Code.Common.rightmost(zipper),
          true <- Igniter.Code.List.list?(zipper),
          {:ok, zipper} <- Igniter.Code.Keyword.get_key(zipper, :mod) do
       case Igniter.Code.Common.expand_literal(zipper) do
