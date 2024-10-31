f9e84cf9136f23f206eec058a99e9e6b4210ab37
Zach Allaun <zach.allaun@gmail.com>
2024-10-31 12:11:12 -0400

    docs: update moduledocs for task info and args

--- lib/mix/task/args.ex
+++ lib/mix/task/args.ex
@@ -1,6 +1,10 @@
 defmodule Igniter.Mix.Task.Args do
   @moduledoc """
   Command line arguments parsed when running an `Igniter.Mix.Task`.
+
+  These args will usually be accessed through `igniter.args` when the
+  `c:Igniter.Mix.Task.igniter/1` callback is run. To learn more about how
+  they are parsed, see `Igniter.Mix.Task.Info`.
   """
 
   defstruct positional: %{}, options: [], argv_flags: [], argv: []
