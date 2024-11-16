db42d660c3041067eb2b88f6b8e4607e859c4b0e
Jonathan Otsuka <dj_goku@Jonathans-MacBook-Pro-3.local>
2024-11-16 16:58:58 -0600

    Add tests for `Igniter.Project.Deps.determine_dep_type_and_version`

--- test/igniter/project/deps_test.exs
+++ test/igniter/project/deps_test.exs
@@ -43,4 +43,39 @@ defmodule Igniter.Project.DepsTest do
       """)
     end
   end
+
+  describe "determine_dep_type_and_version" do
+    test "package" do
+      {package, version_info} = Igniter.Project.Deps.determine_dep_type_and_version("ash")
+      assert package == :ash
+      assert String.contains?(version_info, "~>") == true
+    end
+
+    test "package@version" do
+      assert Igniter.Project.Deps.determine_dep_type_and_version("ash@2") == {:ash, "~> 2.0"}
+    end
+
+    test "package@git" do
+      assert Igniter.Project.Deps.determine_dep_type_and_version(
+               "package@git:https://project/repo.git@git_ref"
+             ) == {:package, [git: "https://project/repo.git", ref: "git_ref", override: true]}
+    end
+
+    test "package@github" do
+      assert Igniter.Project.Deps.determine_dep_type_and_version(
+               "package@github:project/repo@git_ref"
+             ) == {:package, [github: "project/repo", ref: "git_ref", override: true]}
+
+      assert Igniter.Project.Deps.determine_dep_type_and_version(
+               "package@github:project/repo@git/ref/with/slashes/work"
+             ) ==
+               {:package,
+                [github: "project/repo", ref: "git/ref/with/slashes/work", override: true]}
+    end
+
+    test "package@path" do
+      assert Igniter.Project.Deps.determine_dep_type_and_version("package@path:path/to/dep") ==
+               {:package, [path: "path/to/dep", override: true]}
+    end
+  end
 end
