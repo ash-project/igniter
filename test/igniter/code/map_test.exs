4930cd457edcb540e5e6318a295ad1187512b7a4
Nick Krichevsky <nick@ollien.com>
2025-10-29 20:55:29 -0400

    Fix: `put_in_map`/`set_map_key` not setting keys properly (#348)
    * Fix put_key_in_map not setting keys properly
    
    * Fix set_map_key not returning to correct map position consistently
    
    * Fix inserting into nested map in put_in_map
    
    * Fix put_in_map not setting nested keys properly
    
    * License comment
    
    * Use replace_code instead of Zipper.replace

---
+++ test/igniter/code/map_test.exs
@@ -0,0 +1,93 @@
+# SPDX-FileCopyrightText: 2025 igniter contributors <https://github.com/ash-project/igniter/graphs.contributors>
+#
+# SPDX-License-Identifier: MIT
+
+defmodule Igniter.Code.Map.Test do
+  alias Igniter.Code
+  alias Sourceror.Zipper
+
+  use ExUnit.Case
+
+  doctest Igniter.Code.Map
+
+  describe "set_map_key/4" do
+    test "inserts value into empty map" do
+      {:ok, zipper} =
+        "%{}"
+        |> Code.Common.parse_to_zipper!()
+        |> Code.Map.set_map_key(:foo, :bar, fn _valuex -> flunk() end)
+
+      assert {:ok, %{foo: :bar}} == Code.Common.expand_literal(zipper)
+    end
+
+    test "inserts value into map if not present" do
+      {:ok, zipper} =
+        "%{hello: :world}"
+        |> Code.Common.parse_to_zipper!()
+        |> Code.Map.set_map_key(:foo, :bar, &flunk/1)
+
+      assert {:ok, %{foo: :bar, hello: :world}} == Code.Common.expand_literal(zipper)
+    end
+
+    test "replaces keys in map with the updater rather than the passed value" do
+      {:ok, zipper} =
+        "%{hello: :world}"
+        |> Code.Common.parse_to_zipper!()
+        |> Code.Map.set_map_key(:hello, :this_value_is_ignored, fn %Zipper{node: :world} = zipper ->
+          Zipper.replace(zipper, :baz)
+        end)
+
+      assert {:ok, %{hello: :baz}} == Code.Common.expand_literal(zipper)
+    end
+  end
+
+  describe "put_in_map/4" do
+    test "inserts value into empty map" do
+      {:ok, zipper} =
+        "%{}"
+        |> Code.Common.parse_to_zipper!()
+        |> Code.Map.put_in_map([:foo], :bar, fn _valuex -> flunk() end)
+
+      assert {:ok, %{foo: :bar}} == Code.Common.expand_literal(zipper)
+    end
+
+    test "inserts value into empty map at nested position" do
+      {:ok, zipper} =
+        "%{}"
+        |> Code.Common.parse_to_zipper!()
+        |> Code.Map.put_in_map([:alpha, :beta, :gamma], :abc, fn _valuex -> flunk() end)
+
+      assert {:ok, %{alpha: %{beta: %{gamma: :abc}}}} == Code.Common.expand_literal(zipper)
+    end
+
+    test "inserts value into map if not present" do
+      {:ok, zipper} =
+        "%{hello: :world}"
+        |> Code.Common.parse_to_zipper!()
+        |> Code.Map.put_in_map([:foo], :bar, fn _valuex -> flunk() end)
+
+      assert {:ok, %{hello: :world, foo: :bar}} ==
+               Code.Common.expand_literal(zipper)
+    end
+
+    test "inserts nested value into map if not present" do
+      {:ok, zipper} =
+        "%{hello: :world}"
+        |> Code.Common.parse_to_zipper!()
+        |> Code.Map.put_in_map([:foo, :bar], :baz, fn _valuex -> flunk() end)
+
+      assert {:ok, %{hello: :world, foo: %{bar: :baz}}} ==
+               Code.Common.expand_literal(zipper)
+    end
+
+    test "inserts value into map if not present at nested position" do
+      {:ok, zipper} =
+        "%{alpha: %{beta: %{delta: :abd}}}"
+        |> Code.Common.parse_to_zipper!()
+        |> Code.Map.put_in_map([:alpha, :beta, :gamma], :abc, fn _valuex -> flunk() end)
+
+      assert {:ok, %{alpha: %{beta: %{delta: :abd, gamma: :abc}}}} ==
+               Code.Common.expand_literal(zipper)
+    end
+  end
+end
